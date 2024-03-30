28dcaa8b27b239b517ac14e1808164c0dd0ae91d
AustinMcGoey <163172306+AustinMcGoey@users.noreply.github.com>
2024-03-30 13:15:48 -0400

    Testing

---
+++ Group9_MAIN/MainMenu.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+// The user type is the variable used to descide what sub menu is selected
+// UserType 1 = Employee, 2 = Managment and the UserType1 has two sub menus selected through 1 = ______ and 2 = ______ <---- FINISH HERE 
+int UserType = 0;
+int UserType1 = 0;
+
